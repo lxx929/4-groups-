9c9b3909368047fde337b15e0b194f7ba51a4877
lxx <liuxx929@163.com>
2019-04-13 08:03:27 +0800

    commit2

--- my-devser/build/webpack.config.dev.js
+++ my-devser/build/webpack.config.dev.js
@@ -1,15 +1,14 @@
-const base =require('./webpack.config.base')
+const base = require('./webpack.config.base')
 const config = require('../config/index')
 const webpackMerge = require('webpack-merge')
-const configdev={
-    mode:config.dev.mode,
-    devServer:{
-        host:config.dev.host,
-        port:config.dev.port,
-        hot:true,
-        open:true
+const configdev = {
+    mode: config.dev.mode,
+    devServer: {
+        host: config.dev.host,
+        port: config.dev.port,
+        hot: true,
+        open: true
     },
-    devtool:'source-map'
+    devtool: 'source-map'
 }
-module.exports=webpackMerge(configdev,base)
-
+module.exports = webpackMerge(configdev, base)
--- my-devser/src/main.js
+++ my-devser/src/main.js
@@ -1,6 +1,47 @@
 import './css/base.css'
 
-import axios from 'axios'  //es
+// import axios from 'axios' //es
 //  const axios = require('axios')
- let dom = document.getElementById('app')
+// let dom = document.getElementById('app')
 
+
+import './css/base.css'
+
+// import axios from 'axios' //es
+//  const axios = require('axios')
+// let dom = document.getElementById('app');
+
+
+import Vue from 'vue';
+
+new Vue({
+    el: '#app',
+    data: {
+        dogList: [{
+            name: "金毛",
+            isChecked: false
+        }, {
+            name: "二哈",
+            isChecked: false
+        }, {
+            name: "大黄",
+            isChecked: false
+        }, {
+            name: "泰迪",
+            isChecked: false
+        }],
+        isCheckAll: false //全选
+    },
+    methods: {
+        //单选
+        checkAlone(index) {
+            this.dogList[index].isChecked = !this.dogList[index].isChecked; //点击样式
+            this.isCheckAll = this.dogList.every(item => item.isChecked);
+        },
+        //全选
+        checkAll() {
+            this.isCheckAll = !this.isCheckAll; //点击样式
+            this.dogList.forEach(item => item.isChecked = item.isCheckAll);
+        }
+    }
+});
